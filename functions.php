7841c4b3ae85082625c5a40ed51dcfa23a08f419
David Royer <droyer01@gmail.com>
2019-01-15 01:25:46 -0500

    removed unused PHP vars

--- functions.php
+++ functions.php
@@ -1,8 +1,5 @@
 <?php
 
-define('VUE_PORT', 8080);
-define('IS_PRODUCTION', false);
-
 require_once( get_stylesheet_directory() . '/wordpress/remove_redirects.php' );
 require_once( get_stylesheet_directory() . '/wordpress/register_vue.php' );
 require_once( get_stylesheet_directory() . '/wordpress/admin/core.php' );
